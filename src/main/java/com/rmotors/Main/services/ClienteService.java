7fa83900821bb1f260d5f92151e597a06a0993dd
renatossanches <renato.sanches2004@gmail.com>
2024-11-08 16:40:31 -0300

    Criação da classe Service para Cliente e o método "findAll"

---
+++ src/main/java/com/rmotors/Main/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.rmotors.Main.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rmotors.Main.dtos.ClienteDTO;
+import com.rmotors.Main.entities.Cliente;
+import com.rmotors.Main.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public List<ClienteDTO> findAll(){
+		List<Cliente> cli = clienteRepository.findAll(); 
+		return cli.stream().map(this::toClienteDTO).collect(Collectors.toList());
+	}
