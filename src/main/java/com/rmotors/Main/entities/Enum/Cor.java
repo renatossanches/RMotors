82bea36afdfe134d7f7d4d20d1f4bd77511276e0
renatossanches <renato.sanches2004@gmail.com>
2024-11-08 16:26:54 -0300

    Criação das entidades 'Car' e 'Cliente' e suas associações

---
+++ src/main/java/com/rmotors/Main/entities/Enum/Cor.java
@@ -0,0 +1,16 @@
+package com.rmotors.Main.entities.Enum;
+
+public enum Cor {
+
+	PRETO, 
+	BRANCO,
+	VERMELHO,
+	PRATA,
+	CINZA,
+	AZUL,
+	ROSA,
+	AMARELO, 
+	VERDE,
+	MARROM,
+	LARANJA
+}
---
+++ src/test/java/com/rmotorsservicosautomotivos/Main/MainApplicationTests.java
@@ -0,0 +1,19 @@
+package com.rmotorsservicosautomotivos.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+import com.rmotors.Main.entities.Cliente;
+
+@SpringBootTest
+@Configuration
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	
+
+
+	}
+}
