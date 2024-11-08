801c0db601211f831ff531348e4a01d392b00889
renatossanches <renato.sanches2004@gmail.com>
2024-11-08 17:53:33 -0300

    ClienteResource - criação do Insert de cliente

--- src/main/java/com/rmotors/Main/controllers/ClienteResource.java
+++ src/main/java/com/rmotors/Main/controllers/ClienteResource.java
@@ -3,12 +3,16 @@ package com.rmotors.Main.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rmotors.Main.dtos.ClienteDTO;
+import com.rmotors.Main.entities.Cliente;
 import com.rmotors.Main.services.ClienteService;
 
 
@@ -24,5 +28,9 @@ public class ClienteResource {
 		List<ClienteDTO> lista = repo.findAll();
 		return ResponseEntity.ok(lista);
 	}
-	
+	@PostMapping
+	public ResponseEntity<Cliente> insert(@RequestBody Cliente cli){
+		repo.insert(cli);
+		return new ResponseEntity(cli, HttpStatus.CREATED);
+	}
 }
