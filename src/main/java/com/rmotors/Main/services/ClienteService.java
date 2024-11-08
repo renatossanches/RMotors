74341b0eaa46c722b49c0902ef7799e0cac38be7
renatossanches <renato.sanches2004@gmail.com>
2024-11-08 18:16:51 -0300

    ClienteService - criação do findById dos clientes

--- src/main/java/com/rmotors/Main/services/ClienteService.java
+++ src/main/java/com/rmotors/Main/services/ClienteService.java
@@ -1,10 +1,13 @@
 package com.rmotors.Main.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.rmotors.Main.dtos.ClienteDTO;
 import com.rmotors.Main.entities.Cliente;
@@ -25,6 +28,13 @@ public class ClienteService {
 		cli = clienteRepository.save(cli);
 		return cli;
 	}
+	
+	public Cliente findById(@PathVariable String id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.orElseThrow(()-> new ServiceException("Objeto não encontrado"));
+	}
+	
+	
 	
 	public ClienteDTO toClienteDTO(Cliente cli) {
 		ClienteDTO obj = new ClienteDTO(cli.getId(),cli.getNome(),cli.getEmail(),cli.getCar());
